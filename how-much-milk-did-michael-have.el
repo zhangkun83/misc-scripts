972225f30fda09758191a9cfba90d88d46f18f7d
Kun Zhang <arthur.kun@gmail.com>
2022-11-24 13:48:03 -0800

    Improve how-much-milk-did-michale-have.el

--- how-much-milk-did-michael-have.el
+++ how-much-milk-did-michael-have.el
@@ -1,12 +1,14 @@
 (defun how-much-milk-did-michael-have()
-  "Sum up all the numbers in the format like \"2oz\" or \"3.2oz\"
-  within the region"
+  "Sum up all the numbers in the format like \"2 oz\" or
+  \"3.2oz\" within the active region or the whole file"
   (interactive)
   (save-excursion
-    (let ((end (region-end))
-          (sum 0))
-      (goto-char (region-beginning))
-      (while (re-search-forward "\\([0-9.]*\\)oz" end t)
+    (let* ((use-region-p (use-region-p))
+           (end (if use-region-p (region-end) nil))
+           (sum 0))
+      (goto-char (if use-region-p (region-beginning) 0))
+      (while (re-search-forward "\\([0-9.]*\\) *oz" end t)
         (setq sum (+ sum (string-to-number (match-string 1)))))
-      (message "Total milk: %goz" sum))))
-  
+      (message "Total of %s: %g oz"
+               (if use-region-p "region" "whole file")
+               sum))))
