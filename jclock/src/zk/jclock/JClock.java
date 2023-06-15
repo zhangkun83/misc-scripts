8371f4c633d826273b1c53ab8a2cf2746d24ba23
Kun Zhang <arthur.kun@gmail.com>
2023-06-14 18:18:34 -0700

    jclock: add simple alarm function

--- jclock/src/zk/jclock/JClock.java
+++ jclock/src/zk/jclock/JClock.java
@@ -11,10 +11,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 import javax.swing.Timer;
 
@@ -22,18 +26,33 @@ import javax.swing.Timer;
  * A minimalist digital clock that is always visible
  */
 public class JClock extends JFrame {
+  private static final Color BG_NORMAL = Color.BLACK;
+  private static final Color BG_LIGHTED = Color.WHITE;
+  private static final String DATE_FORMAT = "yyyy-MM-dd";
+  private static final String TIME_FORMAT = "HH:mm";
+  private static final DateTimeFormatter dateTimeFormatterDate =
+      DateTimeFormatter.ofPattern(DATE_FORMAT);
+  private static final DateTimeFormatter dateTimeFormatterShort =
+      DateTimeFormatter.ofPattern(TIME_FORMAT);
+  private static final DateTimeFormatter dateTimeFormatterFull =
+      DateTimeFormatter.ofPattern(DATE_FORMAT + " " + TIME_FORMAT);
   private final JLabel content;
+  private final JMenuItem alarmDisplay;
+  private final AlarmNotifier alarmNotifier = new AlarmNotifier();
   private Point mouseClickPoint; // Will reference to the last pressing (not clicking) position
 
   private JClock() {
     // This will make the window skip the window switcher and on all virtual desktops
     setType(Type.UTILITY);
     setLayout(new FlowLayout(FlowLayout.CENTER, 2, 2));
-    getContentPane().setBackground(Color.BLACK);
+    getContentPane().setBackground(BG_NORMAL);
     content = new JLabel("");
     content.setFont(new Font(getFontForSystem(), Font.BOLD, 12));
     content.setForeground(Color.GREEN);
     add(content);
+
+    alarmDisplay = new JMenuItem();
+    alarmDisplay.setEnabled(false);
 
     setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     setAlwaysOnTop(true);
@@ -48,15 +67,66 @@ public class JClock extends JFrame {
     ContentUpdater updater = new ContentUpdater();
     updater.update();
     new Timer(1000, updater).start();
+    new Timer(750, alarmNotifier).start();
     setVisible(true);
+    setAlarm();
+  }
+
+  private void setAlarm() {
+    String input = alarmNotifier.alarm.format(dateTimeFormatterShort);
+    while (true) {
+      input = JOptionPane.showInputDialog(this, "Set Alarm", input);
+      if (input == null) {
+        return;
+      }
+      try {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime newAlarm = LocalDateTime.parse(
+            now.format(dateTimeFormatterDate) + " " + input, dateTimeFormatterFull);
+        if (newAlarm.compareTo(now.minusHours(1)) < 0) {
+          if (JOptionPane.showConfirmDialog(
+                  this,
+                  "The alarm will be set to tomorrow at " + input + ". Are you sure?",
+                  "Set Alarm",
+                  JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+            newAlarm = newAlarm.plusDays(1);
+          } else {
+            return;
+          }
+        }
+        alarmNotifier.alarm = newAlarm;
+        return;
+      } catch (DateTimeParseException e) {
+        JOptionPane.showMessageDialog(
+            this, "Invalid time format: " + e.getMessage(), "Set Alarm", JOptionPane.ERROR_MESSAGE);
+      }
+    }
+  }
+
+  private static String formatTimeForDisplay(LocalDateTime time, LocalDateTime now) {
+    String dateString;
+    String formattedDate = dateTimeFormatterDate.format(time);
+    if (formattedDate.equals(dateTimeFormatterDate.format(now))) {
+      dateString = "";
+    } else if (formattedDate.equals(dateTimeFormatterDate.format(now.plusDays(1)))) {
+      dateString = "Tomorrow";
+    } else {
+      dateString = formattedDate;
+    }
+    return dateString + " " + dateTimeFormatterShort.format(time);
   }
 
   private void addMouseEvents() {
     final JPopupMenu menu = new JPopupMenu();
+    JMenuItem miSetAlarm = new JMenuItem("Set Alarm");
+    miSetAlarm.addActionListener((e) -> setAlarm());
     JMenuItem miClose = new JMenuItem("Close");
     miClose.addActionListener((e) -> System.exit(0));
+    menu.add(miSetAlarm);
+    menu.add(alarmDisplay);
+    menu.addSeparator();
     menu.add(miClose);
-    addMouseListener(new MouseAdapter() {
+    MouseAdapter mouseListener = new MouseAdapter() {
         @Override
         public void mousePressed(MouseEvent e) {
           maybeShowMenu(e);
@@ -73,8 +143,8 @@ public class JClock extends JFrame {
             menu.show(e.getComponent(), e.getX(), e.getY());
           }
         }
-      });
-    addMouseMotionListener(new MouseAdapter() {
+      };
+    MouseAdapter mouseMotionListener = new MouseAdapter() {
         @Override
         public void mouseDragged(MouseEvent e) {
           Point point = e.getLocationOnScreen();
@@ -88,7 +158,11 @@ public class JClock extends JFrame {
               cap(point.getY(), 0, screenSize.getHeight() - size.getHeight()));
           setLocation(point); // set the new location
         }
-      });
+      };
+    addMouseListener(mouseListener);
+    addMouseMotionListener(mouseMotionListener);
+    content.addMouseListener(mouseListener);
+    content.addMouseMotionListener(mouseMotionListener);
   }
 
   private static String getFontForSystem() {
@@ -121,11 +195,30 @@ public class JClock extends JFrame {
     }
 
     void update() {
-      String formatString = showColon ? "hh:mm" : "hh mm";
+      String formatString = showColon ? "HH:mm" : "HH mm";
       content.setText(
           new SimpleDateFormat(formatString).format(new Date()));
       pack();
       showColon = !showColon;
+      alarmDisplay.setText(
+          "Alarm: " + formatTimeForDisplay(alarmNotifier.alarm, LocalDateTime.now()));
+    }
+  }
+
+  private class AlarmNotifier implements ActionListener {
+    boolean lighted;
+    LocalDateTime alarm = LocalDateTime.now();
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+      if (alarmExpired() || lighted) {
+        lighted = !lighted;
+        getContentPane().setBackground(lighted ? BG_LIGHTED : BG_NORMAL);
+      }
+    }
+
+    private boolean alarmExpired() {
+      return LocalDateTime.now().compareTo(alarm) > 0;
     }
   }
 
